e2a53580488cd7c44b51f3392d0ba158f2886095
hotoo <hotoo.cn@gmail.com>
2014-09-17 15:14:18 +0800

    Update implementation via style and :before and :after class.

--- examples/index.md
+++ examples/index.md
@@ -4,12 +4,73 @@
 
 ## Normal usage
 
+<style>
+ul.columns, ul.columns>li{
+  list-style:none;
+  margin: 0;
+  padding:0
+}
+ul.columns>li{
+  padding: 10px;
+  float: left;
+}
+ul.columns:after{
+  content:"\0020"; display:block; height:0; clear:both;
+}
+</style>
+
 ````html
-<div id="name" class="round-percent">5%</div>
+<ul class="columns">
+  <li>
+    <div id="percent0">0%</div>
+  </li>
+  <li>
+    <div id="percent10">0%</div>
+  </li>
+  <li>
+    <div id="percent20">0%</div>
+  </li>
+  <li>
+    <div id="percent25">0%</div>
+  </li>
+  <li>
+    <div id="percent30">0%</div>
+  </li>
+  <li>
+    <div id="percent40">0%</div>
+  </li>
+  <li>
+    <div id="percent45">0%</div>
+  </li>
+  <li>
+    <div id="percent50">0%</div>
+  </li>
+  <li>
+    <div id="percent60">0%</div>
+  </li>
+  <li>
+    <div id="percent70">0%</div>
+  </li>
+  <li>
+    <div id="percent75">0%</div>
+  </li>
+</ul>
 ````
 
 ````javascript
 seajs.use(['index', 'index.css'], function(RoundPercent) {
+
+  new RoundPercent("#percent0", "0%", 100).render();
+  new RoundPercent("#percent10", "10%", 100).render();
+  new RoundPercent("#percent20", "20%", 100).render();
+  new RoundPercent("#percent25", "25%", 100).render();
+  new RoundPercent("#percent30", "30%", 100).render();
+  new RoundPercent("#percent40", "40%", 100).render();
+  new RoundPercent("#percent45", "45%", 100).render();
+  new RoundPercent("#percent50", "50%", 100).render();
+  new RoundPercent("#percent60", "60%", 100).render();
+  new RoundPercent("#percent70", "70%", 100).render();
+  new RoundPercent("#percent75", "75%", 100).render();
 
 });
 ````
--- index.js
+++ index.js
@@ -1,3 +1,84 @@
-var roundPercent;
 
-module.exports = roundPercent;
+var $ = require("jquery");
+
+var offsetDeg = 45; // start from top(90deg)
+
+// @param {String} percent like `5%`
+// @return {Number} like `0.05`
+function parsePercent(percent){
+  return parseFloat(percent) / 100;
+}
+
+function formatPercent(percent){
+  return parseInt(percent * 100, 10) + "%";
+}
+
+function calcDeg(percent){
+  return 360 * percent;
+}
+
+// @param {HTMLElement,jQuery} element
+// @param {String} percent like `5%`
+// @param {Number} width
+// @param {Number} height
+function RoundPercent(element, percent, width, height) {
+  this._element_id = element;
+  this._element = $(element);
+  this._percent = percent;
+  this._width = width;
+  this._height = height || width;
+}
+
+RoundPercent.prototype.render = function(){
+  var element = this._element;
+
+  if (!this._style) {
+    this._style = $('<style></style>').appendTo("head")
+  }
+
+  element.addClass("round-percent")
+    .width(this._width)
+    .height(this._height)
+    .html(this._percent);
+
+  var deg = calcDeg(parsePercent(this._percent));
+  var css;
+
+  if (deg > 360) {
+    return;
+  } else if (deg >= 270){
+    css = [
+      this._element_id + '.round-percent:after{',
+        'border-right:5px solid #999;',
+        'border-bottom:5px solid #999;',
+      '}'];
+  } else if (deg >= 180){
+    css = [
+      this._element_id + '.round-percent:after{',
+        'border-right:5px solid #999;',
+        'border-bottom:5px solid #999;',
+      '}',
+      this._element_id + '.round-percent:before{',
+        'transform:rotate(' + (deg + offsetDeg) + 'deg);',
+      '}'
+      ];
+  } else if (deg >= 90){
+    css = [
+      this._element_id + '.round-percent:after{',
+        'border-right:5px solid #999;',
+      '}',
+      this._element_id + '.round-percent:before{',
+        'transform:rotate(' + (deg + offsetDeg) + 'deg);',
+      '}'
+      ];
+  } else {
+    css = [
+      this._element_id + '.round-percent:before{',
+        'transform:rotate(' + (deg + offsetDeg) + 'deg);',
+      '}'
+    ]
+  }
+  this._style.text( css.join("") );
+};
+
+module.exports = RoundPercent;
